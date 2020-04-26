75fb905c04c39cc3ad9f6be3109b9bb1234dfcc4
Christian Villareal <58633744+christianvillareal@users.noreply.github.com>
2020-04-26 16:00:39 +0800

    Add files via upload

---
+++ assets/js/about.js
@@ -0,0 +1,89 @@
+﻿// Animated Tabs
+$("#tile-1 .nav-tabs a").click(function () {
+    var position = $(this).parent().position();
+    var width = $(this).parent().width();
+    $("#tile-1 .slider").css({ "left": +position.left, "width": width });
+});
+var actWidth = $("#tile-1 .nav-tabs").find(".active").parent("li").width();
+var actPosition = $("#tile-1 .nav-tabs .active").position();
+$("#tile-1 .slider").css({ "left": +actPosition.left, "width": actWidth });
+
+
+// script for flipping text
+(function () {
+    var Flip;
+
+    Flip = class Flip {
+        constructor(el) {
+            this.el = el;
+            this.el = $(this.el);
+            this.currentStep = 0;
+            console.log("Created new Flip");
+            $('.next').on('click', $.proxy(this.next, this));
+        }
+
+        next(event) {
+            var currentStepEl, nextStepEl, nextStepNum;
+            if (event) {
+                event.preventDefault();
+            }
+            nextStepNum = this.currentStep + 1;
+            currentStepEl = this.el.find(`.step${this.currentStep}`);
+            nextStepEl = this.el.find(`.step${nextStepNum}`);
+            if (nextStepEl.length) {
+                console.log('we found the next step', nextStepEl);
+                currentStepEl.prev().removeClass('down');
+                currentStepEl.removeClass('set');
+                currentStepEl.addClass('down');
+                nextStepEl.addClass('set');
+                nextStepEl.removeClass('down');
+                nextStepEl.next().removeClass('down');
+                return this.currentStep++;
+            } else {
+                // reset to 0
+                this.el.find(".step").removeClass('set');
+                this.el.find(`.step${this.currentStep}`).addClass('down');
+                this.el.find(".step").not(`.step${this.currentStep}`).removeClass('down');
+                this.currentStep = -1;
+                return this.next();
+            }
+        }
+    };
+
+
+
+    $(function () {
+        var f;
+        f = new Flip(document.getElementById('flipper'));
+        return setInterval(function () {
+            return f.next();
+        }, 1500);
+    });
+
+}).call(this);
+
+
+// Animated skills bar
+$("#profile-tab").click(function () {
+    $(".skills").fadeIn();
+});
+
+
+$(".skills").addClass("active")
+$(".skills .skill .skill-bar span").each(function () {
+    $(this).animate({
+        "width": $(this).parent().attr("data-bar") + "%"
+    }, 1000);
+    $(this).append('<b>' + $(this).parent().attr("data-bar") + '%</b>');
+});
+setTimeout(function () {
+    $(".skills .skill .skill-bar span b").animate({ "opacity": "1" }, 1000);
+}, 2000);
+
+// Flaslight Animated script
+const gradient = document.querySelector(".sec-1");
+
+function onMouseMove(event) {
+    gradient.style.backgroundImage = 'radial-gradient(at ' + event.clientX + 'px ' + event.clientY + 'px, rgba(0, 144, 236, 0.7) 0, rgba(20, 65, 111, 0.95) 50%)';
+}
+document.addEventListener("mousemove", onMouseMove);
---
+++ assets/js/main.js
@@ -0,0 +1,108 @@
+﻿// scroll down animate script
+$(window).scroll(function () {
+    var sc = $(window).scrollTop()
+    if (sc > 100) {
+        $("#header-sroll").addClass("small")
+    } else {
+        $("#header-sroll").removeClass("small")
+    }
+});
+
+
+// Type Writing Script
+var TxtType = function (el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function () {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+    }
+
+    setTimeout(function () {
+        that.tick();
+    }, delta);
+};
+
+window.onload = function () {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i = 0; i < elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+            new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #969696}";
+    document.body.appendChild(css);
+};
+
+// scroll top script for side menu
+$(function () {
+    var $win = $(window);
+
+    $win.scroll(function () {
+        if ($win.height() + $win.scrollTop() == $(document).height()) {
+            // condition kung naa na sa bottom
+            $("#social_side_links").fadeOut();
+        } else if ($win.scrollTop() + $win.height() < $(document).height() - 200) {
+            // condition kung mo scroll padung top
+            $("#social_side_links").fadeIn();
+        }
+    });
+});
+
+// this code will add animated spinner on click button
+$("#gform").submit(function (e) {
+    e.preventDefault();
+
+    var $send = $("#send-button")
+    $send.removeClass("fa-paper-plane").addClass("fa-spinner fa-pulse")
+
+    setTimeout(function () {
+        for (var i = 0; i < 70000; i++) {
+            console.log(i);
+        }
+        $send.addClass("fa-check").removeClass("fa-spinner fa-pulse fa-spin")
+        return true;
+    }, 50);
+})
+
+
+
+// Load Header
+$("#header-nav").load("/header.html");
+
+// Load Footer
+$("#footer-content").load("/footer.html");
+
